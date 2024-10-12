9bcc432d521ca41b194dac9fcad5d003eba1b8ba
PedroOSPaiva <pedrohenriqueospaiva@gmail.com>
2024-10-12 18:02:16 -0300

    Adiciona o arquivo PrimeiroProjetoRuby.rb

---
+++ PrimeiroProjetoRuby.rb
@@ -0,0 +1,15 @@
+# 1º parte do Projeto deve pegar o nome, sobre e idade do cliente
+
+print "Digite seu nome: "
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp
+
+puts "Leitor: #{nome} #{sobrenome}, Idade: #{idade}"
